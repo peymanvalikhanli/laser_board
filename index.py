fa6053a2f07fa8c1e96081d5123d0882ef490758
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2019-08-21 23:57:43 +0430

    test

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def window():
    b.setText("Hello World!")
    w.setGeometry(100,100,200,50)
    b.move(50,20)
-   w.setWindowTitle(“PyQt”)
+   w.setWindowTitle(“test”)
    w.show()
    sys.exit(app.exec_())
 	
